de0a4a14b56b2a3286f641a6ea76230ff8d74aaa
daniel71-n <h38c@protonmail.com>
2021-02-03 16:04:36 +0100

    Initial commit for an explicit Binary Heap implementation.
    The code is broken and incomplete, at this stage, serving
    just as a savepoint, as it were

---
+++ minheap_ex.c
@@ -0,0 +1,381 @@
+
+
+/* *********************** IMPLEMENTATION NOTES ************************ */
+/* --------------------------------------------------------------------- */
+/* The heap, as implemented here, relies on the structure of a binary tree.
+   That is to say, the nodes in the heap are linked together explicitly
+   by means of pointers, rather than implicitly - when the heap is stored
+   compactly, as an implicit data structure, inside an array.
+
+   There two additional constraints to be complied with for a binary tree
+   to be a heap:
+   - the shape property : the heap is an almost complete binary tree, with
+   new nodes having to be added as far left as possible, until the last 
+   level is filled, before starting a new level. In other words, insertions
+   are done at the bottom, from left to right. 
+   - the heap property: this is the relative ordering of the heap, where
+   each node is either greater than its children (in a max heap) or smaller
+   than them (in a min heap).
+
+   The two 'properties' above, while straightforward to understand, pose
+   perhaps surprisingly challenging issues to explicit, binary-tree -based
+   heap implementations, as discussed below. 
+
+
+   ------------------Explicit and Implicit Heaps--------------------------
+   It's safe to say Heaps are usually implemented as implicit data structures
+   inside an array, where the heap operations manipulate the array indexes
+   mathematically, to modify the array in place. 
+   This is as opposed the explicit implementation of the heap, where use
+   of pointers is involved in a binary-tree structure, which is comparatively
+   only very rarely the approach taken, in practice.
+
+   While the implicit implementation does have its own issues - mainly 
+   having to grow the array when new items are inserted into the heap (
+   which is a costly operation) - the reason heaps are all but always
+   implemented implicitly rather than explicitly becomes apparent
+   when trying to take the latter approach. 
+
+
+   --------------Explicit Heap Implementation Challenges-----------------
+
+   The issue of having to resize the array aside, array-based heaps afford
+   constant time insertion of new nodes at the end of the array. 
+   This is the fundamental issue, really. Since array manipulation
+   boils down to indexes, and since the relationship between the nodes
+   in an array-stored heap can be determined easily in terms of indexes,
+   one can easily determine the position of the last heap node. 
+   This is of the utmost importance, because various fundamental operations
+   are centered around it: removing the root replaces it with the last node,
+   which is then 'sifted down'; inserting a new node is done at the open
+   slot following the last-added node, and then the new node is 'sifted up'.
+
+   The last sentence encapsulates both, on the one hand, the ease and 
+   convenience of implementing a heap as an array-based structure, and
+   on the other hand, the difficulties and pains of doing it in a
+   binary-tree context.
+
+   Knowing the position of the last-added item, the next open slot is
+   the one immediately following it. 
+   In an array, the former is easily determined, and the latter is 
+   simply x+1, where x is the index of the last-added item. 
+   In a tree, it's comparatively more difficult to find the last-added
+   node, and arguably even more so finding the open slot after it,
+   where the new node is supposed to be inserted. 
+
+   The 'shape property' of the heap thus proves difficult to maintain. 
+   
+   There are a few things that could be done, ranging in complexity
+   and overhead. 
+
+   Probably the most obvious solution would be to traverse the tree
+   breadth-first. But this might mean unacceptable or prohibitive 
+   overhead in the case of a very large heap: building a queue data
+   structure where, on by one, all the nodes are enqueued, then
+   examining each node until the free slot is found. This free slot
+   will invariably be found at the end of the traversing the queue,
+   which means you ALWAYS have to carry out a complete traversal
+   going over every single node. Knowing this, you could simply
+   enqueue all the nodes, but jump at some calculated point deep
+   into the queue, where you know the last level of the tree is
+   supposed to start. This notwithstanding, this is an expensive 
+   option.
+
+   You could try and keep a pointer tracking the last-added node, 
+   and then starting from there, find the next free slot. 
+   But this pointer adds space overhead, has to be updated every
+   time an insertion occurs, and solves only half of the problem:
+   you still have to find the next free slot from there, which 
+   is easier said than done. 
+   A related, more full-fledged, alternative is to use a threaded
+   binary tree where the leaves have pointers to their successor/
+   predecessor. This would again though add substantial overhead
+   and complexity as the threaded-tree apparatus has to be 
+   maintained.
+
+   After figuring these two out, and are in a position where you
+   can efficiently find the slot where a new node needs to go in,
+   you then have to 'sift up' or 'percolate up' this node, so as
+   to find its correct position in the heap and abide by the 
+   'heap property'.
+   That is to say, a node is swapped with its parent if its value
+   is larger (max-heap) or smaller (min-heap).
+
+   Each parent has a pointer to each one of its children, but
+   this relationship is one-way only: a child has no pointer to
+   its parent. 
+   To move a node UP the heap then, you need the parent of said node.
+   You could write a routine tracking the node immediately preceding
+   the last node, and then you could compare and potentially swap
+   the newly-inserted node with its parent. 
+   But you need not only its immediate parent, but all the nodes
+   on the way to root, potentially. 
+   This means that this routine would have to be called 
+   repeatedly (a number of times proportional to the height of
+   the tree) each time the node has moved up one position, in order
+   to find its next parent up the tree. Needless to say, this would
+   make sift-up a very costly operation.
+   A better solution, but which would add space overhead, would be
+   to have each node also have a pointer to its parent, making
+   movement up and down the tree trivial.
+
+   
+   ---------- Specific Implementation -------------------------
+
+   No matter the route taken, there's going to be some overhead
+   involved, and normally some time-space tradeoffs. 
+
+   This specific implementation doesn't add extra 'parent'
+   pointers to the nodes, nor does it keep a 'last' pointer,
+   though these would both (especially the former) be valid
+   options, depending on the implementation and its goals.
+
+   Instead it relies on the idea that, like in an array, the
+   next free slot can be determined mathematically, and the
+   same applies to the position of the parent of any node. 
+
+   Given a heap of 7 items, the next slot, will of course, be
+   slot 7+1, just like in an array. 
+   This answers part of the question. The part it doesn't answer
+   is how you find slot 7+1.
+   This can be easily determined as well, based on the observation
+   that the position of each parent is equal to the position of its
+   child divided by two, disregarding any remainder. 
+
+   (Node positions are numbered breadth-first, from left to right).
+                       1
+                    2     3
+                   4 5   6 7
+    The parent of node seven is 7/2 = 3 (remainder of 1, dropped). 
+
+    Building on this, the WHOLE PATH from this node to the root can
+    be traced, by combining these observations.
+    1. Each new insertion happens at n+1, where n is the total number
+    of nodes in the heap. 
+    So given the heap above where n=7, a new node '8' will be inserted 
+    at n+1 = 8. 
+    2. The parent of '8' will be at 8 / 2 = 4, and the parent of 4 will
+    be at 4/2 = 2, and the parent of 2 will be at 2/2 = 1, which is root. 
+    
+    Therefore the path from root to '8' is 1-2-4-8. 
+    As can be seen above, this is all the way to the left down the tree.
+    All the divisions above left no remainder. This is important: if the 
+    division leaves no remained, then the number is even, and starting at
+    root, the path goes the left. If there's a remainder of 1, the path
+    is to the right, on the other hand.
+
+    So the modulo operator will come in handy here, for tracing the path
+    to the node starting at the root.
+    Alternatively, you can use bitwise shifting. It will probably not lead
+    to better performance though, as the compiler is most likely already
+    using this for the modulo operation anyway.
+
+    Given the same example above, you can trace the path from root to node '8'
+    as follows. 
+    8 in binary: 1000
+    You can use a mask of 0001 and AND the two. If the bitwise result is a 1,
+    you go right, while if it's a 0, you go left. 
+    Then shift 8 left by one.
+    Notice that when you've reached the slot, '8' still has a bit (the left-most 1)
+    left in it. This isn't necessary, since you've already reached the end
+    of the path. So the left-most bit is to be disregarded.
+
+    ***********************************************
+    This specific implementation uses the approach above to find the next free 
+    slot for insertions, and then trace the path from root to this node. 
+    This path is needed twice, in opposite directions: coming down from root
+    to find the slot to insert the new node, and going back up the path
+    during sift-up, after the node is inserted. 
+
+    To avoid computing the path twice, a stack data structure is used. 
+    stack1 holds the path down, stack2 the path back up, which is simply
+    the path down, flipped upside down. 
+    compute the path with bitwise shift:
+    push0, push0, push1, for example.
+    
+    stack1                      stack2
+    1  pop1 - go right - push1   0          
+    0  pop0 - go left  - push0   0
+    0  pop0 - go left  - push0   1
+
+    Going back up:
+    stack 2:
+    0
+    0
+    1
+    pop0, pop0,pop1
+    
+    The above is just very simplified pseudocode, to give an idea. 
+    In the actual implementation 1 means a pointer to the right child,
+    and 0 means a pointer to the left child. That means the each child
+    has a pointer to the parent above it by popping this pointer off the
+    stack. 
+    The path from root to the next free slot is therefore created and 
+    kept on the stack, as a series of pointers. 
+
+    To avoid having to count the number of nodes in the tree for every 
+    insertion (necessary for finding the next free slot), there are two
+    separate structs being used: one for heap nodes, which has a key and 
+    right and left pointers, and a separate struct for the heap itself,
+    having a pointer to the root of the heap, and a count member, storing
+    the number of nodes and thus making determining the size a matter of
+    looking it up rather than computing it every time.
+
+    ----------------------CONCLUSION----------------------------------
+
+    Implementing a heap explicitly as a binary tree is something
+    that's very rarely done in practice, and there are good reasons
+    for that. It does have the advandate of being able to better
+    visualize the heap structure (which is why heaps are often shown
+    in tree form in pictorials) as well as being able to grow the 
+    heap without having to resize an array. 
+
+    On the flip side, the fundamental heap operations are more complicated
+    and awkward to reason about in a tree-based heap, specifically
+    finding the next free slot for new node insertions and finding
+    the next parent up the tree for the sift-up routine.
+
+    *********************************************************************   
+*/
+
+
+#include "minheap_ex.h"
+#include <stdlib.h>
+#include "stack.h"
+
+// structure of a Min-Heap node
+struct minheap{
+    HeapNode *root;
+    uint16_t count;
+};
+
+struct minheapnode{
+    char data;
+    HeapNode *left;
+    HeapNode *right;
+};
+
+
+
+uint16_t Heap_count(Heap *the_heap){
+    return the_heap->count;
+};
+
+
+
+static void Heap_sift_down();
+
+
+
+
+
+
+static Stack *Heap_find_spot_P(Heap *the_heap){
+    /* Returns NULL if the last level is filled, which 
+       means it's safe to go one level deeper when inserting
+       the new node, without compromising the heap property.
+    */
+
+    uint16_t slot = the_heap->count + 1;  // the empty slot is at position n+1, where n is the number of nodes in the heap
+    Stack *way_down;   
+    Stack_init(&way_down);
+    // trace the path from the root to the free slot by pushing each node along the path
+    // on the stack. They can then be popped one by one to get from
+    // the root to the free slot
+
+
+    // push root onto the stack first 
+    StackItem *node = malloc(sizeof(StackItem));
+    node->contents = the_heap->root;    // contents is a void pointer, so it can point to anything
+    Stack_push(way_down, node);     // push the node onto the way_down stack
+
+    
+    uint8_t temp;   // used to test the result of the bit shift in the while loop below
+    HeapNode *current = the_heap->root;
+    // rightmost bit in 'slot' isn't needed when shifting, because we want not the exact
+    // slot, but its parent
+    slot >>= 1;
+    
+    while (slot > 1){  // > than 1 because the leftmost bit in heap_size needs to be disregarded
+        temp = slot | 1;
+        node = malloc(sizeof(StackItem));
+
+        if (temp == 0){
+            node->contents = current->left; 
+        }else{
+            node->contents = current->right;
+        };
+        slot >>= 1;
+        Stack_push(way_down, node);
+    };
+    return way_down;    
+};
+
+
+
+static void Heap_sift_up(Stack *path_to_root, HeapNode *current){
+    HeapNode *parent = (Stack_pop(path_to_root))->contents;
+    
+    do{
+        if (current->data < parent->data){
+            char temp = parent->data;
+            parent->data = current->data;
+            current->data = temp;
+
+            current = parent;
+            parent = (Stack_pop(path_to_root))->contents;
+        }
+    }while (Stack_count(path_to_root));
+
+};
+
+
+
+Heap *Heap_insert(Heap *the_heap, char the_value){
+    // find slot where the value needs to be inserted
+    // such that the shape property of the heap is respected
+    Stack *path_to_slot = Heap_find_spot_P(the_heap);
+    HeapNode *slot_parent = (Stack_peek(path_to_slot))->contents; // implcit conversion of void pointer to HeapNode pointer
+    
+    // create new heap node
+    HeapNode *newnode = malloc(sizeof(HeapNode));
+    if (newnode){
+        newnode->data = the_value;
+        newnode->left = newnode->right = NULL;
+
+        if (slot_parent->left == NULL){
+            slot_parent->left = newnode;
+        }
+        else if (slot_parent->right == NULL){
+            slot_parent->right = newnode;
+        }
+
+        // sift up the new node
+
+        // flip the stack upside down
+        path_to_slot = Stack_upend(path_to_slot);
+        Heap_sift_up(path_to_slot, newnode);
+    };
+};
+    
+
+
+
+char Heap_pop_root(Heap *the_heap);
+
+
+
+
+
+
+
+/*
+void Heap_insert(Heap *the_heap, char the_value);
+char Heap_delete(Heap *the_heap, char the_value);
+char Heap_pop_root(Heap *the_heap);
+char Heap_peek_root(Heap *the_heap);
+void Heap_maxheapify(Heap *the_heap);
+void Heap_minheapify(Heap *the_heap);
+void Heap_destroy(Heap **heap_ref);
+*/
+
---
+++ minheap_ex.h
@@ -0,0 +1,50 @@
+
+
+/* ***************************************** Notes ******************************** *
+ * ----------------------------------------------------------------------------------
+ *
+ * This is an implementation of a min-heap. That is, each parent is smaller than both
+ * of its child nodes, going back to root. This means that the root of the heap
+ * has the smallest value.
+ *
+ * Note that a heap is only a partially-ordered data structure. That is to say,
+ * aside from the heap property where the child nodes are smaller than the parent
+ * (max-heap) or greater than it (min-heap), there's no particular order among
+ * sibling nodes. For a sorted data structure, a BST is the way to go. 
+ *
+ * In this implementation the heap is an EXPLICIT data structure, being built 
+ * as a binary tree structure, using pointers to link the heap elements together.
+ * This is in contrast to the heap as an IMPLICIT data structure: that is to say,
+ * heaps can and are often stored implicitly in an array, especially for use in
+ * the heapsort algorithm, where they thus entail no additional overhead beyond
+ * the constant space complexity of the array itself. 
+ *
+ * ******************************************************************************** */
+
+
+
+/* ******* Includes ****** */
+#include <stdint.h>
+
+
+
+
+
+/* ******* Typedefs ******* */
+typedef struct minheap Heap;
+typedef struct minheapnode HeapNode;
+
+
+
+
+/* ******** Function prototypes ********* */
+uint16_t Heap_count(Heap *the_heap);
+Heap *Heap_insert(Heap *the_heap, char the_value);
+char Heap_delete(Heap *the_heap, char the_value);
+char Heap_pop_root(Heap *the_heap);
+char Heap_peek_root(Heap *the_heap);
+void Heap_maxheapify(Heap *the_heap);
+void Heap_minheapify(Heap *the_heap);
+void Heap_destroy(Heap **heap_ref);
+
+
